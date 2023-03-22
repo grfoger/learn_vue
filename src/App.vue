357a9d042b2ecb7fdd37e2926bbe4e693de3d528
grfoger <grfoger@gmail.com>
2023-03-22 15:10:57 +0300

    init vue commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="app">
+    <post-form @create="createPost"/>
+    <post-list :posts="posts"/>
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+  export default {
+    components: {
+      PostList, PostForm
+    },
+    data() {
+      return {
+        posts: [
+          {id:1, title: "JS", body: "Description"},
+          {id:2, title: "JS2", body: "Description2"},
+          {id:3, title: "JS3", body: "Description3"},
+        ],
+      }
+    },
+    methods: {
+        createPost(post) {
+          this.posts.push(post);
+        },
+    }
+  }
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создать пост</h4>
+    <input v-model="post.title"
+           class="input"
+           type="text"
+           placeholder="Название">
+    <input v-model="post.body"
+           class="input"
+           type="text"
+           placeholder="Описание">
+    <button class="btn" @click="createPost">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      }
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post)
+      this.post = {
+        title: '',
+        body: '',
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.input {
+  width: 80%;
+  border: 1px solid forestgreen;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn {
+  margin-top: 15px;
+  align-self: center;
+  padding: 8px 20px;
+  background: none;
+  border: 1px solid forestgreen;
+}
+</style>
