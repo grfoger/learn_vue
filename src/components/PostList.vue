c4c23ec90513a4a48d937e3cb8a70d66831d6e72
grfoger <grfoger@gmail.com>
2023-03-24 17:08:26 +0300

    add pages function

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,13 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Пользователи</h3>
-    <post-item class="post"
-               :post="post"
-               v-for="post in posts"
-               :key="post.id"
-                @remove="$emit('remove', post)"/>
+    <transition-group name="post-list">
+      <post-item class="post"
+                 :post="post"
+                 v-for="post in posts"
+                 :key="post.id"
+                 @remove="$emit('remove', post)"/>
+    </transition-group>
   </div>
   <h2 style="color: red" v-else>Список пользователей пуст</h2>
 </template>
---
+++ src/components/UI/MyPage.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="page__wrapper" :value="modelValue">
+  <div v-for="page in pages"
+       :key="page"
+       class="page"
+       :class="{
+         'current-page': page === current
+       }"
+       @click="changePage(page)"
+  >{{page}}</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "my-page",
+  props: {
+    pages: {
+      type: Array,
+    },
+    current: {
+      type: Number
+    },
+    modelValue: {
+      type:Number
+    }
+
+  },
+  methods: {
+    changePage(page) {
+      this.$emit('update:modelValue', page)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+
+.current-page {
+  border: 4px solid forestgreen;
+}
+</style>
